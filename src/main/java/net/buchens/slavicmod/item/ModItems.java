68047e379620a8fc0c0fa44b294e26e941925434
buchens <skiper10111@gmail.com>
2023-10-07 20:25:18 +0200

    Added new blocks and items

--- src/main/java/net/buchens/slavicmod/item/ModItems.java
+++ src/main/java/net/buchens/slavicmod/item/ModItems.java
@@ -13,6 +13,16 @@ public class ModItems {
 
     public static final RegistryObject<Item> CHAINMAILBRAID = ITEMS.register("chainmailbraid",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> CHROME_INGOT = ITEMS.register("chrome_ingot",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> MANGANESE_INGOT = ITEMS.register("manganese_ingot",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> NICKEL_INGOT = ITEMS.register("nickel_ingot",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> STEEL_INGOT = ITEMS.register("steel_ingot",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> VANADIUM_INGOT = ITEMS.register("vanadium_ingot",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
