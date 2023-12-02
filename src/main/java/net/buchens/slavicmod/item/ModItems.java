417d4dbc55d6347c9f0265b8b7058e9f3119bbc7
buchens <skiper10111@gmail.com>
2023-12-02 21:50:59 +0100

    dodano 3x komponenty budujące zbroje wilczą, z dzika i niedźwiedzia i craftingi

--- src/main/java/net/buchens/slavicmod/item/ModItems.java
+++ src/main/java/net/buchens/slavicmod/item/ModItems.java
@@ -13,6 +13,36 @@ public class ModItems {
 
     public static final RegistryObject<Item> CHAINMAILBRAID = ITEMS.register("chainmailbraid",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> WOLF_FUR = ITEMS.register("wolf_fur",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> WOLF_HELMET_COMPONENT = ITEMS.register("wolf_helmet_component",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> WOLF_CHESTPLATE_COMPONENT = ITEMS.register("wolf_chestplate_component",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> WOLF_LEGGINS_COMPONENT = ITEMS.register("wolf_leggins_component",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> WOLF_BOOTS_COMPONENT = ITEMS.register("wolf_boots_component",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> BEAR_FUR = ITEMS.register("bear_fur",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> BEAR_BOOTS_COMPONENT = ITEMS.register("bear_boots_component",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> BEAR_LEGGINS_COMPONENT = ITEMS.register("bear_leggins_component",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> BEAR_CHESTPLATE_COMPONENT = ITEMS.register("bear_chestplate_component",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> BEAR_HELMET_COMPONENT = ITEMS.register("bear_helmet_component",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> BOAR_FUR = ITEMS.register("boar_fur",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> BOAR_HELMET_COMPONENT = ITEMS.register("boar_helmet_component",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> BOAR_CHESTPLATE_COMPONENT = ITEMS.register("boar_chestplate_component",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> BOAR_LEGGINS_COMPONENT = ITEMS.register("boar_leggins_component",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
+    public static final RegistryObject<Item> BOAR_BOOTS_COMPONENT = ITEMS.register("boar_boots_component",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
     public static final RegistryObject<Item> STEEL_WIRE = ITEMS.register("steel_wire",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SLAVICCRAFT_TAB)) );
     public static final RegistryObject<Item> CHROME_INGOT = ITEMS.register("chrome_ingot",
